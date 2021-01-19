451184a459833a460e6fd496a18d16556d69959f
junjun2712 <35761057+junjun2712@users.noreply.github.com>
2021-01-20 04:12:47 +0800

    Create py_query_es.py

---
+++ py_query_es.py
@@ -0,0 +1,29 @@
+# encoding:utf-8
+import json
+import re
+import os
+from elasticsearch import  Elasticsearch
+from bson.json_util import dumps
+
+
+#es地址
+es=Elasticsearch(["172.18.230.84:9200"])
+
+#查询内容
+addr = "172.18.230.222"
+
+body={"query":{"bool":{"must":{"match":{"message":addr}},"filter":{"range":{"@timestamp":{"gte":"now-1m/m","lte":"now/m","format":"epoch_millis"}}}}}}
+
+data=es.search(index="qp-betinfo*",body=body) #指定索引和查询内容
+#print(type(data))
+
+
+fh=str(data)
+#print fh
+#print (type(fh))
+
+str_a = fh.split("value\':")[-1]
+#print(str_a)
+
+str_b = str_a.split("}")[0]
+print(str_b)
